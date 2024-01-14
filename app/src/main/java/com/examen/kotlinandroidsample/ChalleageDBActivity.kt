7c253d86a31d69c8518e95c209f6e7ec4d684534
sainathhiwale <sainathhiwale2@gmail.com>
2024-01-14 19:58:26 +0530

    second data binding sample is added

---
+++ app/src/main/java/com/examen/kotlinandroidsample/ChalleageDBActivity.kt
@@ -0,0 +1,39 @@
+package com.examen.kotlinandroidsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.examen.kotlinandroidsample.databinding.ActivityChalleageDbactivityBinding
+
+class ChalleageDBActivity : AppCompatActivity(), View.OnClickListener {
+    // auto create the data binding object based on layout file
+    private lateinit var binding: ActivityChalleageDbactivityBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // init the data binding object the DataBindingUtil.setContentView method is used to set the content view of the activity and create the binding object.
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_challeage_dbactivity)
+        //setContentView(R.layout.activity_challeage_dbactivity)
+        initView()
+    }
+
+    private fun initView() {
+        binding.submitButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id==R.id.submit_Button){
+            startorStopProgressBar()
+        }
+    }
+
+    private fun startorStopProgressBar() {
+        if (binding.progressBar.visibility== View.GONE){
+            binding.progressBar.visibility = View.VISIBLE
+            binding.submitButton.text = "Stop"
+        }else{
+            binding.progressBar.visibility = View.GONE
+            binding.submitButton.text = "Start"
+        }
+    }
+}

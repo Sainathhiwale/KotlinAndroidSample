42d96cf81bdde525802fc8a5f603d27f9cdfe7ac
sainathhiwale <sainathhiwale2@gmail.com>
2024-01-14 16:09:13 +0530

    data binding sample is added

--- app/src/main/java/com/examen/kotlinandroidsample/MainActivity.kt
+++ app/src/main/java/com/examen/kotlinandroidsample/MainActivity.kt
@@ -2,15 +2,29 @@ package com.examen.kotlinandroidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.examen.kotlinandroidsample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private var activityMainBinding:ActivityMainBinding?=null
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-       /* activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        val view = activityMainBinding!!.root
-        setContentView(view)*/
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        activityMainBinding.submitButton.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id==R.id.submit_Button){
+           displayMessage()
+        }
+    }
+
+    private fun displayMessage() {
+        activityMainBinding.textView.text = "Welcome:" + activityMainBinding.editText.text
     }
 }
